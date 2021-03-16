cc7257568675a430d80f56c3655cf5af0e673950
wqy1998 <wqy_19980810@163.com>
2021-03-16 22:21:10 +0800

    add check cookies

--- src/main/java/com/wqy/campusbbs/controller/HomeController.java
+++ src/main/java/com/wqy/campusbbs/controller/HomeController.java
@@ -1,12 +1,32 @@
 package com.wqy.campusbbs.controller;
 
+import com.wqy.campusbbs.mapper.UserMapper;
+import com.wqy.campusbbs.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "home";
     }
 }
