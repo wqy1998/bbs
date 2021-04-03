5de9d950ded7a522c316b742168c64116b50ea4d
wqy1998 <wqy_19980810@163.com>
2021-04-03 23:57:31 +0800

    增加富文本编辑；实现首页问题描述字符大于30时省略其后字符

--- src/main/java/com/wqy/campusbbs/controller/HomeController.java
+++ src/main/java/com/wqy/campusbbs/controller/HomeController.java
@@ -1,12 +1,15 @@
 package com.wqy.campusbbs.controller;
 
 import com.wqy.campusbbs.dto.PaginationDTO;
+import com.wqy.campusbbs.model.Question;
 import com.wqy.campusbbs.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
